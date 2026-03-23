63d6f22d99a0dd7c883347f49bc0f52f8b2609b1
Alex-Endevour <alejugger@gmail.com>
2026-03-22 20:29:30 -0600

    Implement initial home page structure and styling

--- src/home.js
+++ src/home.js
@@ -2,9 +2,11 @@ export function loadHome() {
   const content = document.getElementById('content');
   content.innerHTML = '';
 
-  // TODO: build your home page here using DOM methods
-  // e.g.:
-  // const h1 = document.createElement('h1');
-  // h1.textContent = 'Welcome to My Restaurant!';
-  // content.appendChild(h1);
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Welcome to My Restaurant';
+  content.appendChild(h1);
+
+  const p = document.createElement('p');
+  p.textContent = 'Best food in town.';
+  content.appendChild(p);
 }
